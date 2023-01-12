8d4359dc9214a571d5c8234544105fdc309f7c37
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-12 10:23:18 -0500

    [add] seeder

--- database/migrations/2023_01_12_143149_create_comics_table.php
+++ database/migrations/2023_01_12_143149_create_comics_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 110)->unique();
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->float('price', 4, 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
