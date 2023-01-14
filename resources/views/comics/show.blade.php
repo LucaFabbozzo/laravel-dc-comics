c7f175ffc604a2f01290bf33a986eb7b81f43678
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-14 08:54:57 -0500

    corrections

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
     <div class="container container-lf">
+        <a href="{{route('comics.index')}}" class="btn btn-outline-secondary text-uppercase my-2">Back to the List</a>
         <div class="card m-auto p-2 mb-5" style="width: 50%;">
          <img style="width: 40%" src="{{$comic->image}}" class="card-img-top m-auto p-2 bg-dark" alt="{{$comic->title}}">
             <div class="card-body">
@@ -11,7 +12,7 @@
                 <p class="card-title fs-4">Series: {{$comic->series}}</p>
                 <p class="card-title fs-6">&euro; {{$comic->price}}</p>
                 <p class="card-text fs-6">{{$comic->description}}</p>
-                <a href="{{route('comics.index')}}" class="btn btn-outline-secondary text-uppercase">Back</a>
+                <a class="btn btn-outline-success" href="{{route('comics.edit', $comic)}}"><i class="fa-regular fa-pen-to-square"></i> Modify</a>
             </div>
         </div>
     </div>
